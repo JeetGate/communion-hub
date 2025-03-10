140d60cdf745c4ea8de5b4701347d980fc6606ea
Jeet <jeetgate@gmail.com>
2025-03-10 22:42:16 +0530

    Updated footer text

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer style={{ textAlign: "center", padding: "10px 0" }}>
+        <p>Made with ❤️ by Jeet Gate</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
